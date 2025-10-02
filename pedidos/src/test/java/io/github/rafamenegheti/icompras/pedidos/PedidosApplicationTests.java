7ac0fcdbf508074e2ec7db0769c7fc95c9bca517
Rafael Menegheti <rafaelmenegheti51@gmail.com>
2025-10-02 03:23:57 -0300

    feat: adding pedidos microservice structure

---
+++ pedidos/src/test/java/io/github/rafamenegheti/icompras/pedidos/PedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rafamenegheti.icompras.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
